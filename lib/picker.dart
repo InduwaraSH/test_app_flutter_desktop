3eadb36012cf45a3c950c0aadf164c7ff2c176bc
Induwara <dolee@Induwaras-MacBook-Air.local>
2025-08-01 03:30:50 +0530

    add picker page and update register ui

---
+++ lib/picker.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Picker extends StatefulWidget {
+  const Picker({super.key});
+
+  @override
+  State<Picker> createState() => _PickerState();
+}
+
+class _PickerState extends State<Picker> {
+  int _selectedTown = 0;
+  int _selectedTown_Drop = 0;
+  static const double _kItemExtent = 32.0;
+  static const List<String> _townName = <String>[
+    'Maharagama',
+    'Embilipitya',
+    'Matara',
+    'Galle',
+    'Colombo',
+    'Trinco',
+    'Walasmulla',
+    'Negambo',
+  ];
+
+  void _showDialog(Widget child) {
+    showCupertinoModalPopup<void>(
+      context: context,
+      builder: (BuildContext context) => Container(
+        height: 216,
+        padding: const EdgeInsets.only(top: 6.0),
+        margin: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        color: CupertinoColors.systemBackground.resolveFrom(context),
+        child: SafeArea(top: false, child: child),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(15.0),
+          child: Expanded(
+            child: DefaultTextStyle(
+              style: TextStyle(
+                color: CupertinoColors.label.resolveFrom(context),
+                fontSize: 22.0,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      CupertinoButton(
+                        padding: EdgeInsets.zero,
+                        // Display a CupertinoPicker with list of Stands.
+                        onPressed: () => _showDialog(
+                          CupertinoPicker(
+                            magnification: 1.22,
+                            squeeze: 1.2,
+                            useMagnifier: true,
+                            itemExtent: _kItemExtent,
+                            // This sets the initial item.
+                            scrollController: FixedExtentScrollController(
+                              initialItem: _selectedTown,
+                            ),
+                            // This is called when selected item is changed.
+                            onSelectedItemChanged: (int selectedItem) {
+                              setState(() {
+                                _selectedTown = selectedItem;
+                              });
+                            },
+                            children: List<Widget>.generate(_townName.length, (
+                              int index,
+                            ) {
+                              return Center(child: Text(_townName[index]));
+                            }),
+                          ),
+                        ),
+                        // This displays the selected fruit name.
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(5.0),
+                          child: Container(
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                            padding: EdgeInsets.only(
+                              left: 20.0,
+                              right: 20.0,
+                              top: 5.0,
+                              bottom: 5.0,
+                            ),
+                            child: Text(
+                              _townName[_selectedTown],
+                              style: const TextStyle(
+                                fontSize: 20.0,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/registration.dart
+++ lib/registration.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:test_code/picker.dart';
 
 class RegistrationPage extends StatelessWidget {
   const RegistrationPage({super.key});
@@ -7,85 +8,220 @@ class RegistrationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Theme.of(context).colorScheme.surface,
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(height: 50),
-              Text(
-                "Registration",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontFamily: 'sfpro',
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).colorScheme.outline,
-                ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                width: (MediaQuery.of(context).size.width - 100) * 0.82,
-
-                child: Form(
-                  autovalidateMode: AutovalidateMode.always,
-                  onChanged: () {
-                    Form.maybeOf(primaryFocus!.context!)?.save();
-                  },
-                  child: CupertinoFormSection.insetGrouped(
-                    backgroundColor: Theme.of(context).colorScheme.secondary,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.secondary,
-                      borderRadius: BorderRadius.circular(26),
-                    ),
-                    header: Text(
-                      'Branch details',
-                      style: TextStyle(
-                        fontFamily: 'sfpro',
-
-                        color: Theme.of(context).colorScheme.outline,
-                      ),
-                    ),
-                    children: [
-                      CupertinoFormRow(
-                        prefix: Text(
-                          'Branch Id',
-                          style: TextStyle(
-                            fontFamily: 'sfpro',
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).colorScheme.outline,
-                          ),
-                        ),
-                        child: CupertinoTextFormFieldRow(
-                          cursorColor: Colors.black,
-                          placeholder: 'Enter branch id',
-                          onSaved: (value) {
-                            print(value);
-                          },
-                        ),
-                      ),
-                      CupertinoFormRow(
-                        prefix: Text(
-                          'Bus Number',
-                          style: TextStyle(
-                            fontFamily: 'sfpro',
-
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).colorScheme.outline,
-                          ),
-                        ),
-                        child: CupertinoTextFormFieldRow(
-                          cursorColor: Theme.of(context).colorScheme.outline,
-                          placeholder: 'Enter bus number',
-                          onSaved: (value) {
-                            print(value);
-                          },
-                        ),
-                      ),
-                    ],
+      body: SingleChildScrollView(
+        child: Container(
+          color: Theme.of(context).colorScheme.surface,
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(height: 50),
+                Text(
+                  "Registration",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontFamily: 'sfpro',
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.outline,
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 40),
+                Container(
+                  width: (MediaQuery.of(context).size.width - 100) * 0.82,
+
+                  child: Form(
+                    autovalidateMode: AutovalidateMode.always,
+                    onChanged: () {
+                      Form.maybeOf(primaryFocus!.context!)?.save();
+                    },
+                    child: CupertinoFormSection.insetGrouped(
+                      backgroundColor: Theme.of(context).colorScheme.secondary,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.secondary,
+                        borderRadius: BorderRadius.circular(26),
+                      ),
+                      header: Text(
+                        'Branch details',
+                        style: TextStyle(
+                          fontFamily: 'sfpro',
+
+                          color: Theme.of(context).colorScheme.outline,
+                        ),
+                      ),
+                      children: [
+                        CupertinoFormRow(
+                          prefix: Text(
+                            'Branch Id',
+                            style: TextStyle(
+                              fontFamily: 'sfpro',
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).colorScheme.outline,
+                            ),
+                          ),
+                          child: CupertinoTextFormFieldRow(
+                            cursorColor: Colors.black,
+                            placeholder: 'Enter branch id',
+                            onSaved: (value) {
+                              print(value);
+                            },
+                          ),
+                        ),
+                        CupertinoFormRow(
+                          prefix: Text(
+                            'Branch Location',
+                            style: TextStyle(
+                              fontFamily: 'sfpro',
+
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).colorScheme.outline,
+                            ),
+                          ),
+                          child: Row(children: [SizedBox(width: 30), Picker()]),
+                        ),
+                        CupertinoFormRow(
+                          prefix: Text(
+                            'Branch Password',
+                            style: TextStyle(
+                              fontFamily: 'sfpro',
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).colorScheme.outline,
+                            ),
+                          ),
+                          child: CupertinoTextFormFieldRow(
+                            cursorColor: Colors.black,
+                            placeholder: 'Enter branch password',
+                            onSaved: (value) {
+                              print(value);
+                            },
+                          ),
+                        ),
+                        CupertinoFormRow(
+                          prefix: Text(
+                            'Branch Password',
+                            style: TextStyle(
+                              fontFamily: 'sfpro',
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).colorScheme.outline,
+                            ),
+                          ),
+                          child: CupertinoTextFormFieldRow(
+                            cursorColor: Colors.black,
+                            placeholder: 'Re Enter branch password',
+                            onSaved: (value) {
+                              print(value);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                //-----------------second container-------------------
+                Container(
+                  width: (MediaQuery.of(context).size.width - 100) * 0.82,
+
+                  child: Form(
+                    autovalidateMode: AutovalidateMode.always,
+                    onChanged: () {
+                      Form.maybeOf(primaryFocus!.context!)?.save();
+                    },
+                    child: CupertinoFormSection.insetGrouped(
+                      backgroundColor: Theme.of(context).colorScheme.secondary,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.secondary,
+                        borderRadius: BorderRadius.circular(26),
+                      ),
+                      header: Text(
+                        'Manager details',
+                        style: TextStyle(
+                          fontFamily: 'sfpro',
+
+                          color: Theme.of(context).colorScheme.outline,
+                        ),
+                      ),
+                      children: [
+                        CupertinoFormRow(
+                          prefix: Text(
+                            'Manager Id',
+                            style: TextStyle(
+                              fontFamily: 'sfpro',
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).colorScheme.outline,
+                            ),
+                          ),
+                          child: CupertinoTextFormFieldRow(
+                            cursorColor: Colors.black,
+                            placeholder: 'Enter branch id',
+                            onSaved: (value) {
+                              print(value);
+                            },
+                          ),
+                        ),
+                        CupertinoFormRow(
+                          prefix: Text(
+                            'Manager Name',
+                            style: TextStyle(
+                              fontFamily: 'sfpro',
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).colorScheme.outline,
+                            ),
+                          ),
+                          child: CupertinoTextFormFieldRow(
+                            cursorColor: Colors.black,
+                            placeholder: 'Enter branch id',
+                            onSaved: (value) {
+                              print(value);
+                            },
+                          ),
+                        ),
+
+                        CupertinoFormRow(
+                          prefix: Text(
+                            'Manager Password',
+                            style: TextStyle(
+                              fontFamily: 'sfpro',
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).colorScheme.outline,
+                            ),
+                          ),
+                          child: CupertinoTextFormFieldRow(
+                            cursorColor: Colors.black,
+                            placeholder: 'Enter manager password',
+                            onSaved: (value) {
+                              print(value);
+                            },
+                          ),
+                        ),
+                        CupertinoFormRow(
+                          prefix: Text(
+                            'Manager Password',
+                            style: TextStyle(
+                              fontFamily: 'sfpro',
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).colorScheme.outline,
+                            ),
+                          ),
+                          child: CupertinoTextFormFieldRow(
+                            cursorColor: Colors.black,
+                            placeholder: 'Re Enter manager password',
+                            onSaved: (value) {
+                              print(value);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 50),
+                CupertinoButton(
+                  color: CupertinoColors.black,
+
+                  onPressed: () {},
+                  child: Text('Submit', style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
           ),
         ),
       ),
