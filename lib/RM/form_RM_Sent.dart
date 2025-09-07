6e2c9743b998e652d7c9c1d0c1e9b447f66a9b71
InduwaraSH <s.h.induwara.dil@gmail.com>
2025-09-07 16:52:36 +0530

    updated rm sent animation

--- lib/RM/form_RM_Sent.dart
+++ lib/RM/form_RM_Sent.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:test_code/ARM_SelectProvider.dart';
@@ -20,7 +19,6 @@ class Sent_Form_RM_new extends StatefulWidget {
 
 class _Sent_Form_RMState extends State<Sent_Form_RM_new> {
   String? savedValue;
-
   late String location;
   late String position;
 
@@ -49,123 +47,117 @@ class _Sent_Form_RMState extends State<Sent_Form_RM_new> {
   Widget build(BuildContext context) {
     final rmSent = Provider.of<RM_Sent>(context);
 
-    return Container(
-      width: double.infinity,
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color(0xFFF3F2FD), // soft lavender background
-            Color(0xFFE9E8FB),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: SingleChildScrollView(
-        child: Visibility(
-          visible: rmSent.selected ?? true,
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
+    // Collect data cards
+    final cardData = [
+      {
+        "icon": Icons.confirmation_number_outlined,
+        "label": "Serial No",
+        "value": rmSent.s_num.toString(),
+      },
+      {
+        "icon": Icons.place_outlined,
+        "label": "Place of Coupe",
+        "value": rmSent.poc.toString(),
+      },
+      {
+        "icon": Icons.numbers_outlined,
+        "label": "Letter No",
+        "value": rmSent.letter_no.toString(),
+      },
+      {
+        "icon": Icons.date_range_outlined,
+        "label": "Date informed",
+        "value": rmSent.date_informed.toString(),
+      },
+    ];
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFF4F2FA),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(20),
+          child: Visibility(
+            visible: rmSent.selected ?? true,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                // Print button row
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: const [_HoverPrintButton(), SizedBox(width: 20)],
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    CircleAvatar(
+                      radius: 24,
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        Icons.account_circle_rounded,
+                        color: Color(0xFF6C63FF),
+                        size: 40,
+                      ),
+                    ),
+                    Spacer(),
+                    _HoverPrintButton(),
+                  ],
                 ),
-
+                const SizedBox(height: 20),
+                Text(
+                  "TO : ARM Branch in ${rmSent.arm_branch_name.toString()}",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey.shade800,
+                  ),
+                ),
+                Text(
+                  "From : RM Branch in $location",
+                  style: TextStyle(fontSize: 14, color: Colors.grey.shade600),
+                ),
                 const SizedBox(height: 30),
-
-                // Title
-                Center(
-                  child: Text(
-                    "Enumeration And Wayside \n Deport Register For Donated Timber.",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontFamily: 'DMSerif',
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromRGBO(101, 89, 182, 1),
-                    ),
-                  ),
-                ),
-
-                const SizedBox(height: 50),
-
-                // From - To Section
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "From :   RM Branch in $location",
-                        style: const TextStyle(
-                          fontFamily: 'DMSerif',
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromRGBO(117, 106, 182, 1),
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        "To      :   ARM Branch in ${rmSent.arm_branch_name.toString()}",
-                        style: const TextStyle(
-                          fontFamily: 'DMSerif',
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromRGBO(117, 106, 182, 1),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-                const SizedBox(height: 50),
-
-                // Focused Info Card
                 Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 20),
-                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(22),
                   decoration: BoxDecoration(
-                    color: const Color.fromRGBO(232, 230, 255, 0.5),
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(
-                      color: const Color.fromRGBO(145, 142, 214, 0.8),
-                      width: 1.8,
+                    gradient: LinearGradient(
+                      colors: [Colors.white, const Color(0xFFF1EEFF)],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
                     ),
+                    borderRadius: BorderRadius.circular(24),
                     boxShadow: [
                       BoxShadow(
-                        color: const Color.fromRGBO(145, 142, 214, 0.15),
+                        color: Colors.black.withOpacity(0.05),
                         blurRadius: 12,
                         offset: const Offset(0, 6),
                       ),
                     ],
                   ),
-                  child: Column(
-                    children: [
-                      _InfoRowTile(
-                        label: "Serial No",
-                        value: rmSent.s_num.toString(),
+                  child: Center(
+                    child: Text(
+                      "Enumeration And Wayside Deport Register\nFor Donated Timber",
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontSize: 25,
+                        fontFamily: "DMSerif",
+                        fontWeight: FontWeight.w800,
+                        color: Color.fromARGB(255, 45, 39, 169),
                       ),
-                      _InfoRowTile(
-                        label: "Place of Coupe",
-                        value: rmSent.poc.toString(),
-                      ),
-                      _InfoRowTile(
-                        label: "Letter No",
-                        value: rmSent.letter_no.toString(),
-                      ),
-                      _InfoRowTile(
-                        label: "Date informed",
-                        value: rmSent.date_informed.toString(),
-                      ),
-                    ],
+                    ),
                   ),
                 ),
-
                 const SizedBox(height: 30),
+
+                /// Animated cards section
+                Column(
+                  children: List.generate(cardData.length, (index) {
+                    final data = cardData[index];
+                    return _SequentialDropCard(
+                      icon: data["icon"] as IconData,
+                      label: data["label"] as String,
+                      value: data["value"] as String,
+                      delay: Duration(
+                        milliseconds: 150 * index,
+                      ), // stagger effect
+                    );
+                  }),
+                ),
               ],
             ),
           ),
@@ -175,18 +167,110 @@ class _Sent_Form_RMState extends State<Sent_Form_RM_new> {
   }
 }
 
-/// Hoverable info row
-class _InfoRowTile extends StatefulWidget {
+/// Card that drops sequentially with delay
+class _SequentialDropCard extends StatefulWidget {
+  final IconData icon;
   final String label;
   final String value;
-  const _InfoRowTile({Key? key, required this.label, required this.value})
-    : super(key: key);
+  final Duration delay;
+
+  const _SequentialDropCard({
+    Key? key,
+    required this.icon,
+    required this.label,
+    required this.value,
+    required this.delay,
+  }) : super(key: key);
 
   @override
-  State<_InfoRowTile> createState() => _InfoRowTileState();
+  State<_SequentialDropCard> createState() => _SequentialDropCardState();
 }
 
-class _InfoRowTileState extends State<_InfoRowTile> {
+class _SequentialDropCardState extends State<_SequentialDropCard>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _offsetAnimation;
+  bool _visible = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 600),
+      vsync: this,
+    );
+
+    _offsetAnimation = Tween<Offset>(
+      begin: const Offset(0, -0.8),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutBack));
+
+    // Trigger the animation with delay
+    Future.delayed(widget.delay, () {
+      if (mounted) {
+        setState(() => _visible = true);
+        _controller.forward();
+      }
+    });
+  }
+
+  @override
+  void didUpdateWidget(covariant _SequentialDropCard oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.value != widget.value) {
+      // Reset visibility for new data
+      _controller.reset();
+      setState(() => _visible = false);
+
+      Future.delayed(widget.delay, () {
+        if (mounted) {
+          setState(() => _visible = true);
+          _controller.forward();
+        }
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _visible
+        ? SlideTransition(
+            position: _offsetAnimation,
+            child: _DataCard(
+              icon: widget.icon,
+              label: widget.label,
+              value: widget.value,
+            ),
+          )
+        : const SizedBox.shrink();
+  }
+}
+
+/// Data card with hover effect
+class _DataCard extends StatefulWidget {
+  final IconData icon;
+  final String label;
+  final String value;
+
+  const _DataCard({
+    Key? key,
+    required this.icon,
+    required this.label,
+    required this.value,
+  }) : super(key: key);
+
+  @override
+  State<_DataCard> createState() => _DataCardState();
+}
+
+class _DataCardState extends State<_DataCard> {
   bool _hovered = false;
 
   @override
@@ -194,37 +278,68 @@ class _InfoRowTileState extends State<_InfoRowTile> {
     return MouseRegion(
       onEnter: (_) => setState(() => _hovered = true),
       onExit: (_) => setState(() => _hovered = false),
-      child: TweenAnimationBuilder<double>(
-        duration: const Duration(milliseconds: 220),
+      child: AnimatedScale(
+        scale: _hovered ? 1.03 : 1.0,
+        duration: const Duration(milliseconds: 200),
         curve: Curves.easeInOut,
-        tween: Tween(begin: 1.0, end: _hovered ? 1.02 : 1.0),
-        builder: (context, scale, child) {
-          return Transform.scale(
-            scale: scale,
-            alignment: Alignment.centerLeft,
-            child: AnimatedContainer(
-              duration: const Duration(milliseconds: 220),
-              curve: Curves.easeInOut,
-              padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 20),
-              margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
-              decoration: BoxDecoration(
-                color: _hovered
-                    ? const Color.fromRGBO(210, 206, 255, 0.6)
-                    : Colors.transparent,
-                borderRadius: BorderRadius.circular(12),
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 20),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.white, const Color(0xFFF4F0FF)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(22),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.05),
+                blurRadius: 12,
+                offset: const Offset(0, 6),
               ),
-              child: Text(
-                "${widget.label} :   ${widget.value}",
-                style: const TextStyle(
-                  color: Color.fromRGBO(74, 64, 134, 1),
-                  fontFamily: 'RoboSerif',
-                  fontWeight: FontWeight.w700,
-                  fontSize: 18,
+            ],
+          ),
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 24,
+                backgroundColor: const Color(0xFFEAE6FF),
+                child: Icon(
+                  widget.icon,
+                  color: const Color(0xFF5C50C5),
+                  size: 26,
                 ),
               ),
-            ),
-          );
-        },
+              const SizedBox(width: 18),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.label,
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      widget.value,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontFamily: "AbhayaLibre",
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -248,63 +363,41 @@ class _HoverPrintButtonState extends State<_HoverPrintButton> {
     return MouseRegion(
       onEnter: (_) => setState(() => _hovered = true),
       onExit: (_) => setState(() => _hovered = false),
-      child: TweenAnimationBuilder<double>(
-        duration: const Duration(milliseconds: 220),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 700),
         curve: Curves.easeInOut,
-        tween: Tween(begin: 1.0, end: _hovered ? 1.12 : 1.0),
-        builder: (context, scale, child) {
-          return Transform.scale(
-            scale: scale,
-            child: Container(
-              width: 44,
-              height: 44,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                gradient: LinearGradient(
-                  colors: _hovered
-                      ? const [
-                          Color.fromRGBO(125, 110, 210, 1),
-                          Color.fromRGBO(160, 155, 230, 1),
-                        ]
-                      : const [
-                          Color.fromRGBO(145, 142, 214, 1),
-                          Color.fromRGBO(177, 175, 255, 1),
-                        ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: const Color.fromRGBO(117, 106, 182, 0.35),
-                    blurRadius: _hovered ? 18 : 10,
-                    offset: const Offset(0, 5),
-                  ),
-                ],
-              ),
-              child: IconButton(
-                icon: const Icon(
-                  Icons.local_print_shop_outlined,
-                  color: Colors.white,
-                  size: 24,
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => PdfStyledPage(
-                        rmSent.s_num.toString(),
-                        rmSent.poc.toString(),
-                        rmSent.letter_no.toString(),
-                        rmSent.date_informed.toString(),
-                      ),
-                    ),
-                  );
-                },
-                tooltip: 'Print',
-              ),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: LinearGradient(
+            colors: _hovered
+                ? [const Color(0xFF6C63FF), const Color(0xFF928DFF)]
+                : [const Color(0xFF928DFF), const Color(0xFFB8B5FF)],
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.15),
+              blurRadius: _hovered ? 16 : 8,
+              offset: const Offset(0, 5),
             ),
-          );
-        },
+          ],
+        ),
+        child: IconButton(
+          icon: const Icon(Icons.print_outlined, color: Colors.white, size: 24),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => PdfStyledPage(
+                  rmSent.s_num.toString(),
+                  rmSent.poc.toString(),
+                  rmSent.letter_no.toString(),
+                  rmSent.date_informed.toString(),
+                ),
+              ),
+            );
+          },
+          tooltip: 'Print',
+        ),
       ),
     );
   }
